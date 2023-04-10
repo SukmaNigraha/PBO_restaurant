22bd1e09e3240ca8ccb00df947fb0f8f614ed95f
SukmaNigraha <sukmanigraha87@gmail.com>
2023-04-10 20:10:14 +0800

    adding lefted file

---
+++ src/first_package/login.java
@@ -0,0 +1,41 @@
+package first_package;
+
+public class login {
+    private String username_admin,password_admin,
+
+                   username_user,password_user;
+    public login(){
+        username_admin = "sugma";
+        password_admin = "haimanis";
+
+        username_user = "sigma";
+        password_user = "haimanis";
+    }
+    //setter
+
+    public void setUsername_admin(String username_admin) {
+        this.username_admin = username_admin;}
+
+    public void setPassword_admin(String password_admin) {
+        this.password_admin = password_admin;}
+
+    public void setUsername_user(String username_user) {
+        this.username_user = username_user;}
+
+    public void setPassword_user(String password_user) {
+        this.password_user = password_user;}
+    //getter
+
+    public String getUsername_admin() {
+        return username_admin;}
+
+    public String getPassword_admin() {
+        return password_admin;}
+
+    public String getUsername_user() {
+        return username_user;}
+
+    public String getPassword_user() {
+        return password_user;}
+}
+
